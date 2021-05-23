f2505a659a32c4fc324ff83fd3afcc36a54bd35e
mechanic <sreenath@shiftbytes.co.in>
2021-05-23 13:47:54 +0530

    changed from functions to class.

--- cookie_management.js
+++ cookie_management.js
@@ -1,33 +1,28 @@
-function CookieBytes() {
-	const self = {
-		set :  (cname, cvalue, exdays) => {
-			var d = new Date();
-			d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-			var expires = "expires="+d.toUTCString();
-			document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-		},
-
-		get : (cname) => {
-			var name = cname + "=";
-			var ca = document.cookie.split(';');
-			for(var i = 0; i < ca.length; i++) {
-				var c = ca[i];
-				while (c.charAt(0) == ' ') {
-					c = c.substring(1);
-				}
-				if (c.indexOf(name) == 0) {
-					return c.substring(name.length, c.length);
-				}
-			}
-			return "";
-		},
-
-		delete : (cname) => {
-			document.cookie = cname + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-		} 
+class CookieBytes {
+	set =  (cname, cvalue, exdays) => {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 	}
-
-	return self;
+	get = (cname) => {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return "";
+	}
+	del = (cname) => {
+		document.cookie = cname + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+	} 
 }
 
-module.exports.CookieBytes = CookieBytes;
+
+module.exports.cookiebytes = CookieBytes;
